3525bec02f74abf55f8d01305a78568bea05f10c
Muzammil Hussain <muzammil.hussain@gharpar.co>
2020-03-26 18:33:35 +0500

    Route Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="container">
-          <Movies />
-        </div>
-      </React.Fragment>
+      <div className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
     );
   }
 }
