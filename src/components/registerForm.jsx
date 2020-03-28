1ee1e3091ca5bd89ecb5a91049a9e69865d402fa
Muzammil Hussain <muzammil.hussain@gharpar.co>
2020-03-28 22:18:32 +0500

    Register form component initiated

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  render() {
+    return null;
+  }
+}
+
+export default RegisterForm;
