a75e5cf0bd46dcf58be5b8d7eef3e6a58e2ab585
Muzammil Hussain <muzammil.hussain@gharpar.co>
2020-04-04 22:33:58 +0500

    Extrat and clean the auth dependency

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,14 +1,40 @@
+import jwtDecode from "jwt-decode";
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
 export async function login(email, password) {
-  await http.post(apiEndpoint, { email, password });
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function logout() {
-  localStorage.removeItem("token");
+  localStorage.removeItem(tokenKey);
 }
 
-export default { login, logout };
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (error) {
+    return null;
+  }
+}
+
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
+
+export default {
+  login,
+  loginWithJwt,
+  logout,
+  getCurrentUser,
+  getJwt,
+};
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import logger from "./logService";
 
-axios.interceptors.response.use(null, error => {
+axios.defaults.headers.common["x-auth-token"] = auth.getJwt();
+
+axios.interceptors.response.use(null, (error) => {
   const expectedError =
     error.response &&
     error.response.status >= 400 &&
@@ -21,5 +23,5 @@ export default {
   post: axios.post,
   put: axios.put,
   patch: axios.patch,
-  delete: axios.delete
+  delete: axios.delete,
 };
